454cc583e6c80fa88671ca7e4c6026eb41a97b7d
Magnitoz <kub99999@yandex.ru>
2021-10-25 17:01:04 +0500

    #7 Create new project TamagotchGame
    - import FormEgg.cs, FormEgg.Designer.cs, FormEgg.resx, Iterator.cs, Program.cs, WorkWithimage.cs

---
+++ TamagotchiGame/FormEgg.Designer.cs
@@ -0,0 +1,294 @@
+﻿
+namespace TamagotchiGame
+{
+    partial class FormEgg
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormEgg));
+            this.pictureBoxScreen = new System.Windows.Forms.PictureBox();
+            this.pictureBoxEat = new System.Windows.Forms.PictureBox();
+            this.pictureBoxLight = new System.Windows.Forms.PictureBox();
+            this.pictureBoxGame = new System.Windows.Forms.PictureBox();
+            this.pictureBoxCure = new System.Windows.Forms.PictureBox();
+            this.pictureBoxClear = new System.Windows.Forms.PictureBox();
+            this.pictureBoxInformation = new System.Windows.Forms.PictureBox();
+            this.pictureBoxEducation = new System.Windows.Forms.PictureBox();
+            this.pictureBoxHappen = new System.Windows.Forms.PictureBox();
+            this.PictureBoxButtonSelect = new System.Windows.Forms.PictureBox();
+            this.pictureBoxButtonDecide = new System.Windows.Forms.PictureBox();
+            this.pictureBoxButtonCancel = new System.Windows.Forms.PictureBox();
+            this.pictureBoxButtonReset = new System.Windows.Forms.PictureBox();
+            this.pictureBoxButtonExit = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxScreen)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxEat)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGame)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCure)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxClear)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxInformation)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxEducation)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxHappen)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBoxButtonSelect)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxButtonDecide)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxButtonCancel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxButtonReset)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxButtonExit)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxScreen
+            // 
+            this.pictureBoxScreen.BackColor = System.Drawing.Color.Black;
+            this.pictureBoxScreen.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBoxScreen.BackgroundImage")));
+            this.pictureBoxScreen.Location = new System.Drawing.Point(77, 190);
+            this.pictureBoxScreen.Name = "pictureBoxScreen";
+            this.pictureBoxScreen.Size = new System.Drawing.Size(246, 120);
+            this.pictureBoxScreen.TabIndex = 0;
+            this.pictureBoxScreen.TabStop = false;
+            // 
+            // pictureBoxEat
+            // 
+            this.pictureBoxEat.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBoxEat.BackgroundImage")));
+            this.pictureBoxEat.Location = new System.Drawing.Point(106, 158);
+            this.pictureBoxEat.Name = "pictureBoxEat";
+            this.pictureBoxEat.Size = new System.Drawing.Size(26, 26);
+            this.pictureBoxEat.TabIndex = 1;
+            this.pictureBoxEat.TabStop = false;
+            // 
+            // pictureBoxLight
+            // 
+            this.pictureBoxLight.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBoxLight.BackgroundImage")));
+            this.pictureBoxLight.Location = new System.Drawing.Point(161, 158);
+            this.pictureBoxLight.Name = "pictureBoxLight";
+            this.pictureBoxLight.Size = new System.Drawing.Size(26, 26);
+            this.pictureBoxLight.TabIndex = 2;
+            this.pictureBoxLight.TabStop = false;
+            // 
+            // pictureBoxGame
+            // 
+            this.pictureBoxGame.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBoxGame.BackgroundImage")));
+            this.pictureBoxGame.Location = new System.Drawing.Point(217, 158);
+            this.pictureBoxGame.Name = "pictureBoxGame";
+            this.pictureBoxGame.Size = new System.Drawing.Size(26, 26);
+            this.pictureBoxGame.TabIndex = 3;
+            this.pictureBoxGame.TabStop = false;
+            // 
+            // pictureBoxCure
+            // 
+            this.pictureBoxCure.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBoxCure.BackgroundImage")));
+            this.pictureBoxCure.Location = new System.Drawing.Point(272, 158);
+            this.pictureBoxCure.Name = "pictureBoxCure";
+            this.pictureBoxCure.Size = new System.Drawing.Size(26, 26);
+            this.pictureBoxCure.TabIndex = 4;
+            this.pictureBoxCure.TabStop = false;
+            // 
+            // pictureBoxClear
+            // 
+            this.pictureBoxClear.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBoxClear.BackgroundImage")));
+            this.pictureBoxClear.Location = new System.Drawing.Point(106, 318);
+            this.pictureBoxClear.Name = "pictureBoxClear";
+            this.pictureBoxClear.Size = new System.Drawing.Size(26, 26);
+            this.pictureBoxClear.TabIndex = 5;
+            this.pictureBoxClear.TabStop = false;
+            // 
+            // pictureBoxInformation
+            // 
+            this.pictureBoxInformation.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBoxInformation.BackgroundImage")));
+            this.pictureBoxInformation.Location = new System.Drawing.Point(161, 318);
+            this.pictureBoxInformation.Name = "pictureBoxInformation";
+            this.pictureBoxInformation.Size = new System.Drawing.Size(26, 26);
+            this.pictureBoxInformation.TabIndex = 6;
+            this.pictureBoxInformation.TabStop = false;
+            // 
+            // pictureBoxEducation
+            // 
+            this.pictureBoxEducation.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBoxEducation.BackgroundImage")));
+            this.pictureBoxEducation.Location = new System.Drawing.Point(217, 318);
+            this.pictureBoxEducation.Name = "pictureBoxEducation";
+            this.pictureBoxEducation.Size = new System.Drawing.Size(26, 26);
+            this.pictureBoxEducation.TabIndex = 7;
+            this.pictureBoxEducation.TabStop = false;
+            // 
+            // pictureBoxHappen
+            // 
+            this.pictureBoxHappen.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBoxHappen.BackgroundImage")));
+            this.pictureBoxHappen.Location = new System.Drawing.Point(272, 318);
+            this.pictureBoxHappen.Name = "pictureBoxHappen";
+            this.pictureBoxHappen.Size = new System.Drawing.Size(26, 26);
+            this.pictureBoxHappen.TabIndex = 8;
+            this.pictureBoxHappen.TabStop = false;
+            // 
+            // PictureBoxButtonSelect
+            // 
+            this.PictureBoxButtonSelect.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("PictureBoxButtonSelect.BackgroundImage")));
+            this.PictureBoxButtonSelect.Location = new System.Drawing.Point(75, 370);
+            this.PictureBoxButtonSelect.Name = "PictureBoxButtonSelect";
+            this.PictureBoxButtonSelect.Size = new System.Drawing.Size(27, 27);
+            this.PictureBoxButtonSelect.TabIndex = 9;
+            this.PictureBoxButtonSelect.TabStop = false;
+            this.PictureBoxButtonSelect.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PictureBoxButtonSelect_MouseDown);
+            this.PictureBoxButtonSelect.MouseUp += new System.Windows.Forms.MouseEventHandler(this.PictureBoxButtonSelect_MouseUp);
+            // 
+            // pictureBoxButtonDecide
+            // 
+            this.pictureBoxButtonDecide.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBoxButtonDecide.BackgroundImage")));
+            this.pictureBoxButtonDecide.Location = new System.Drawing.Point(132, 396);
+            this.pictureBoxButtonDecide.Name = "pictureBoxButtonDecide";
+            this.pictureBoxButtonDecide.Size = new System.Drawing.Size(27, 27);
+            this.pictureBoxButtonDecide.TabIndex = 10;
+            this.pictureBoxButtonDecide.TabStop = false;
+            this.pictureBoxButtonDecide.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PictureBoxButtonDecide_MouseDown);
+            this.pictureBoxButtonDecide.MouseUp += new System.Windows.Forms.MouseEventHandler(this.PictureBoxButtonDecide_MouseUp);
+            // 
+            // pictureBoxButtonCancel
+            // 
+            this.pictureBoxButtonCancel.BackColor = System.Drawing.SystemColors.ControlText;
+            this.pictureBoxButtonCancel.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBoxButtonCancel.BackgroundImage")));
+            this.pictureBoxButtonCancel.Location = new System.Drawing.Point(191, 408);
+            this.pictureBoxButtonCancel.Name = "pictureBoxButtonCancel";
+            this.pictureBoxButtonCancel.Size = new System.Drawing.Size(27, 27);
+            this.pictureBoxButtonCancel.TabIndex = 11;
+            this.pictureBoxButtonCancel.TabStop = false;
+            this.pictureBoxButtonCancel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PictureBoxButtonCancel_MouseDown);
+            this.pictureBoxButtonCancel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.PictureBoxButtonCancel_MouseUp);
+            // 
+            // pictureBoxButtonReset
+            // 
+            this.pictureBoxButtonReset.BackColor = System.Drawing.SystemColors.ControlText;
+            this.pictureBoxButtonReset.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBoxButtonReset.BackgroundImage")));
+            this.pictureBoxButtonReset.Location = new System.Drawing.Point(252, 396);
+            this.pictureBoxButtonReset.Name = "pictureBoxButtonReset";
+            this.pictureBoxButtonReset.Size = new System.Drawing.Size(27, 27);
+            this.pictureBoxButtonReset.TabIndex = 12;
+            this.pictureBoxButtonReset.TabStop = false;
+            this.pictureBoxButtonReset.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PictureBoxButtonReset_MouseDown);
+            this.pictureBoxButtonReset.MouseUp += new System.Windows.Forms.MouseEventHandler(this.PictureBoxButtonReset_MouseUp);
+            // 
+            // pictureBoxButtonExit
+            // 
+            this.pictureBoxButtonExit.BackColor = System.Drawing.SystemColors.ControlText;
+            this.pictureBoxButtonExit.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBoxButtonExit.BackgroundImage")));
+            this.pictureBoxButtonExit.Location = new System.Drawing.Point(299, 371);
+            this.pictureBoxButtonExit.Name = "pictureBoxButtonExit";
+            this.pictureBoxButtonExit.Size = new System.Drawing.Size(27, 27);
+            this.pictureBoxButtonExit.TabIndex = 13;
+            this.pictureBoxButtonExit.TabStop = false;
+            this.pictureBoxButtonExit.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PictureBoxButtonExit_MouseDown);
+            this.pictureBoxButtonExit.MouseUp += new System.Windows.Forms.MouseEventHandler(this.PictureBoxButtonExit_MouseUp);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(94, 94);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 15);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "65656+";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(182, 62);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(61, 24);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // FormEgg
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlText;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(400, 600);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBoxButtonExit);
+            this.Controls.Add(this.pictureBoxButtonReset);
+            this.Controls.Add(this.pictureBoxButtonCancel);
+            this.Controls.Add(this.pictureBoxButtonDecide);
+            this.Controls.Add(this.PictureBoxButtonSelect);
+            this.Controls.Add(this.pictureBoxHappen);
+            this.Controls.Add(this.pictureBoxEducation);
+            this.Controls.Add(this.pictureBoxInformation);
+            this.Controls.Add(this.pictureBoxClear);
+            this.Controls.Add(this.pictureBoxCure);
+            this.Controls.Add(this.pictureBoxGame);
+            this.Controls.Add(this.pictureBoxLight);
+            this.Controls.Add(this.pictureBoxEat);
+            this.Controls.Add(this.pictureBoxScreen);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(5, 3, 5, 3);
+            this.Name = "FormEgg";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.Form_Egg_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form_Egg_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form_Egg_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxScreen)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxEat)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGame)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxCure)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxClear)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxInformation)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxEducation)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxHappen)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBoxButtonSelect)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxButtonDecide)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxButtonCancel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxButtonReset)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxButtonExit)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxScreen;
+        private System.Windows.Forms.PictureBox pictureBoxEat;
+        private System.Windows.Forms.PictureBox pictureBoxLight;
+        private System.Windows.Forms.PictureBox pictureBoxGame;
+        private System.Windows.Forms.PictureBox pictureBoxCure;
+        private System.Windows.Forms.PictureBox pictureBoxClear;
+        private System.Windows.Forms.PictureBox pictureBoxInformation;
+        private System.Windows.Forms.PictureBox pictureBoxEducation;
+        private System.Windows.Forms.PictureBox pictureBoxHappen;
+        private System.Windows.Forms.PictureBox PictureBoxButtonSelect;
+        private System.Windows.Forms.PictureBox pictureBoxButtonDecide;
+        private System.Windows.Forms.PictureBox pictureBoxButtonCancel;
+        private System.Windows.Forms.PictureBox pictureBoxButtonReset;
+        private System.Windows.Forms.PictureBox pictureBoxButtonExit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ TamagotchiGame/WorkWithImage.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace TamagotchiGame
+{
+    sealed class WorkWithImage
+    {
+        private readonly Iterator<PictureBox> _iteratorPictureBox;
+        private readonly Iterator<string> _iteratorButton;
+        private readonly Iterator<string> _iteratorIconAct;
+        private readonly Iterator<string> _iteratorIconBackGround;
+        private readonly Iterator<string> _iteratorIconHappen;
+        private readonly string[] _stringLocation = new string[2];
+        private readonly string[] _stringLocationIconHappen = new string[2];
+        private readonly string[] _stringLocationIconAct= new string[7];
+        private readonly string[] _stringLocationIconBackGround = new string[7];
+        private readonly PictureBox[] _arrayPictureBoxIcons;
+        private PictureBox _lastPictureBox;
+        private bool _lastItemIcon;
+
+        public WorkWithImage(PictureBox[] pictureBoxIcons)
+        {
+            _stringLocation[0] = @"Image/Button/ButtonDown.png";
+            _stringLocation[1] = @"Image/Button/ButtonUp.png";
+
+            _stringLocationIconHappen[0] = @"Image/Icon/HappenAct.png";
+            _stringLocationIconHappen[1] = @"Image/Icon/HappenBackGround.png";
+
+            _stringLocationIconAct[0] = @"Image/Icon/ЕatAct.png";
+            _stringLocationIconAct[1] = @"Image/Icon/LightAct.png";
+            _stringLocationIconAct[2] = @"Image/Icon/GameAct.png";
+            _stringLocationIconAct[3] = @"Image/Icon/CureAct.png";
+            _stringLocationIconAct[4] = @"Image/Icon/CleanAct.png";
+            _stringLocationIconAct[5] = @"Image/Icon/InformationAct.png";
+            _stringLocationIconAct[6] = @"Image/Icon/EducationAct.png";
+            
+            _stringLocationIconBackGround[0] = @"Image/Icon/ЕatBackGround.png";
+            _stringLocationIconBackGround[1] = @"Image/Icon/LightBackGround.png";
+            _stringLocationIconBackGround[2] = @"Image/Icon/GameBackGround.png";
+            _stringLocationIconBackGround[3] = @"Image/Icon/CureBackGround.png";
+            _stringLocationIconBackGround[4] = @"Image/Icon/CleanBackGround.png";
+            _stringLocationIconBackGround[5] = @"Image/Icon/InformationBackGround.png";
+            _stringLocationIconBackGround[6] = @"Image/Icon/EducationBackGround.png";
+
+            _arrayPictureBoxIcons = pictureBoxIcons;
+
+            _iteratorButton = new Iterator<string>(_stringLocation);
+            _iteratorIconHappen = new Iterator<string>(_stringLocationIconHappen);
+            _iteratorIconAct = new Iterator<string>(_stringLocationIconAct);
+            _iteratorIconBackGround = new Iterator<string>(_stringLocationIconBackGround);
+            _iteratorPictureBox = new Iterator<PictureBox>(pictureBoxIcons);
+        }
+
+        public void ChangePictureBox(Object currentObject, MouseButtons leftButton)
+        {
+            if (MouseButtons.Left == leftButton)
+            {
+                PictureBox currentPictureBox = currentObject as PictureBox;
+                string locationFile = NextItem(_iteratorButton);
+                LoadPictureBox(locationFile, currentPictureBox);
+            }
+        }
+
+        private string NextItem(Iterator<string> newItem)
+        {
+            if (newItem.MoveNext() == false)
+            {
+                newItem.Reset();
+                newItem.MoveNext();
+            }
+
+            return newItem.Current;
+        }
+
+        private PictureBox NextItem(Iterator<PictureBox> newItem)
+        {
+            if (newItem.MoveNext() == false)
+            {
+                newItem.Reset();
+                newItem.MoveNext();
+            }
+
+            return newItem.Current;
+        }
+
+        private PictureBox PreviousItem(Iterator<PictureBox> newItem)
+        {
+            if (newItem.MovePrevious() == false)
+            {
+                NextItem(newItem);
+            }
+
+            return newItem.Current;
+        }
+
+        public void ChangeIconHappen(PictureBox currentPictureBox)
+        {
+            string locationFile = NextItem(_iteratorIconHappen);
+            LoadPictureBox(locationFile, currentPictureBox);
+        }
+
+        public void NextIcon()
+        {
+            PictureBox currentPictureBox = _arrayPictureBoxIcons[0];
+            string locationFile;
+
+            if (_lastItemIcon == false)
+            {
+                currentPictureBox = NextItem(_iteratorPictureBox);
+                locationFile = NextItem(_iteratorIconAct);
+                LoadPictureBox(locationFile, currentPictureBox);
+            }
+                
+            if (currentPictureBox.Equals(_arrayPictureBoxIcons[0]) == false)
+            {
+                currentPictureBox = PreviousItem(_iteratorPictureBox);
+                locationFile = NextItem(_iteratorIconBackGround);
+                LoadPictureBox(locationFile, currentPictureBox);
+                currentPictureBox = NextItem(_iteratorPictureBox);
+
+                if (currentPictureBox.Equals(_arrayPictureBoxIcons[6]))
+                {
+                    _lastItemIcon = true;
+                    _lastPictureBox = currentPictureBox;
+                }
+            }
+            else
+            {
+                if (_lastItemIcon)
+                {
+                    currentPictureBox = _lastPictureBox;
+                    locationFile = NextItem(_iteratorIconBackGround);
+                    LoadPictureBox(locationFile, currentPictureBox);
+                    _lastItemIcon = false;
+                }
+            }
+        }
+
+        private void LoadPictureBox(string locationFile, PictureBox currentPictureBox)
+        {
+            try
+            {
+                currentPictureBox.Load(locationFile);
+            }
+            catch (InvalidOperationException)
+            {
+                throw new InvalidOperationException();
+            }
+            catch (FileNotFoundException)
+            {
+                throw new FileNotFoundException();
+            }
+            catch (ArgumentException)
+            {
+                throw new ArgumentException();
+            }
+            catch (NullReferenceException)
+            {
+                throw new ArgumentException();
+            }
+        }
+    }
+}
